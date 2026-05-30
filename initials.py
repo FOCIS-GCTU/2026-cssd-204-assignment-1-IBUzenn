b875d018f5819a8d3729013510a886a07491e82d
IBUzenn <2425404295@live.gctu.edu.gh>
2026-05-30 23:11:35 +0000

    Assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,39 @@
+# File: initials.py
+# Description: Print Out My Initials
+# Assignment Number: 1
+#
+# Name: TERRENCE ANSA-SASRAKU
+# STUDENT ID: 2425404295
+# Email: 2425404295@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, TERRENCE ANSA-SASRAKU, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+# Main function of the program
+
+    # Print a blank line for spacing
+    print("")
+
+    # Print the initials title
+    print('...TAS')
+
+    # Print another blank line before the artwork
+    print("")
+
+    # Print the ASCII art representing the initials T, A, and S
+    print('...TTTTTTTTTTTT.........AAAAAAAAAAAA..........SSSSSSSSSSS....')
+    print('...TTTTTTTTTTTT.........AAAAAAAAAAAA.........SSSSSSSSSSSS....')
+    print('...TTTTTTTTTTTT.........AAAA....AAAA........SSSS.............')
+    print('...TTTTTTTTTTTT.........AAAA....AAAA........SSSS.............')
+    print('.......TTTT.............AAAA....AAAA..........SSSSSS.........')
+    print('.......TTTT.............AAAAAAAAAAAA............SSSSSS.......')
+    print('.......TTTT.............AAAAAAAAAAAA................SSSS.....')
+    print('.......TTTT.............AAAA....AAAA................SSSS.....')
+    print('.......TTTT........**...AAAA....AAAA...**...SSSSSSSSSSS....**')
+    print('.......TTTT........**...AAAA....AAAA...**...SSSSSSSSSS.....**')
 
 
-
+# Call the main function to start the program
 main()
